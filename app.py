eb75c8648eb2bbff1535cd974f87050bfd049a5d
linnchin <LinnKern.Chin@amd.com>
2022-04-10 17:49:06 +0800

    fix bug on db

--- app.py
+++ app.py
@@ -15,13 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'test'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    print('Created Table')
-    db.create_all()
-
-
 jwt = JWT(app=app,
           authentication_handler=authenticate,
           identity_handler=identity)    # /auth
@@ -35,4 +28,13 @@ api.add_resource(UserRegister, '/register')
 if __name__ == "__main__":
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            print('Created Table')
+            db.create_all()
+
+    app.run(port=5000)
+
+
